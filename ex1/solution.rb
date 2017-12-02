32128d38a083f826190ac3a685248fe29a93ec48
Fong Fan <fong.fan.90@gmail.com>
2017-12-01 17:37:22 -0800

    Day 1 completed

--- ex1/solution.rb
+++ ex1/solution.rb
@@ -1,5 +1,4 @@
 def captcha_sum(input)
-  puts input
   digits = input.split('').map(&:to_i)
   sum = 0
 
@@ -11,7 +10,7 @@ def captcha_sum(input)
 end
 
 def solution_1
-  file = './input.txt'
+  file = './input1.txt'
   input = File.read(file).strip
   solution = captcha_sum(input)
 
@@ -21,3 +20,30 @@ def solution_1
   puts "Solution:"
   puts solution
 end
+
+
+def captcha_halfway_sum(input)
+  digits = input.split('').map(&:to_i)
+  sum = 0
+  halfway = digits.length / 2
+
+  digits.each_with_index do |digit, index|
+    sum += digit if digit == digits[index - halfway]
+  end
+
+  sum
+end
+
+def solution_2
+  file = './input2.txt'
+  input = File.read(file).strip
+  solution = captcha_halfway_sum(input)
+
+  puts "Input:"
+  puts input
+
+  puts "Solution:"
+  puts solution
+end
+
+solution_2
--- ex1/test.rb
+++ ex1/test.rb
@@ -42,3 +42,45 @@ describe 'captcha_sum' do
     assert_equal(expected, result)
   end
 end
+
+describe 'captcha_halfway_sum' do
+  it 'returns the sum of all digits when all digits match the digits halfway ahead' do
+    input = '1212'
+    result = captcha_halfway_sum(input)
+    expected = 6
+
+    assert_equal(expected, result)
+  end
+
+  it 'returns 0 because none of the digits match halfway around' do
+    input = '1221'
+    result = captcha_halfway_sum(input)
+    expected = 0
+
+    assert_equal(expected, result)
+  end
+
+  it 'produces 4, because both 2s match each other, but no other digit has a match' do
+    input = '123425'
+    result = captcha_halfway_sum(input)
+    expected = 4
+
+    assert_equal(expected, result)
+  end
+
+  it 'return 12' do
+    input = '123123'
+    result = captcha_halfway_sum(input)
+    expected = 12
+
+    assert_equal(expected, result)
+  end
+
+  it 'returns 4' do
+    input = '12131415'
+    result = captcha_halfway_sum(input)
+    expected = 4
+
+    assert_equal(expected, result)
+  end
+end
